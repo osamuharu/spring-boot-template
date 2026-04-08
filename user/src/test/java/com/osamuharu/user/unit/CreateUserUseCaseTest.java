d156d3d728b4b51a421779c72d092599beb60f81
osamuharu <huynhnamkha1010@gmail.com>
2026-04-09 05:05:29 +0700

    chore: rewrite test for create user

--- user/src/test/java/com/osamuharu/user/unit/CreateUserUseCaseTest.java
+++ user/src/test/java/com/osamuharu/user/unit/CreateUserUseCaseTest.java
@@ -1,5 +1,19 @@
 package com.osamuharu.user.unit;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoInteractions;
+import static org.mockito.Mockito.when;
+
+import com.osamuharu.shared.provider.PasswordProvider;
+import com.osamuharu.user.application.exception.EmailExistsException;
+import com.osamuharu.user.application.exception.UserCannotNullException;
+import com.osamuharu.user.application.exception.UserNameExistsException;
 import com.osamuharu.user.application.usecase.CreateUserUseCase;
 import com.osamuharu.user.domain.entity.User;
 import com.osamuharu.user.domain.repository.UserRepository;
@@ -11,89 +25,98 @@ import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.*;
-
 @ExtendWith(MockitoExtension.class)
 public class CreateUserUseCaseTest {
-	
-	@Mock
-	private UserRepository repository;
-	
-	static private User user;
-	
-	@InjectMocks
-	private CreateUserUseCase createUserUseCase;
-	
-	@BeforeAll
-	static void initAll() {
-		user = User.builder()
-		           .username("johndoe")
-		           .firstName("John")
-		           .lastName("Doe")
-		           .email("johndoe@gmail.com")
-		           .build();
-	}
-	
-	@Test
-	@DisplayName("execute() - when valid user is provided, should save and return the user")
-	public void execute_WhenValidUser_ShouldSaveAndReturnUser() {
-		when(repository.existsByEmail(user.getEmail())).thenReturn(false);
-		when(repository.existsByUsername(user.getUsername())).thenReturn(false);
-		when(repository.save(user)).thenReturn(user);
-		
-		User result = createUserUseCase.execute(user);
-		
-		assertNotNull(result);
-		assertEquals(result, user);
-		
-		verify(repository, times(1)).existsByEmail(user.getEmail());
-		verify(repository, times(1)).existsByUsername(user.getUsername());
-		verify(repository, times(1)).save(user);
-	}
-	
-	@Test
-	@DisplayName("execute() - when user is null, throw IllegalArgumentException('User cannot be null')")
-	public void execute_WhenInValidUser_ShouldSaveAndReturnUser() {
-		IllegalArgumentException exception = assertThrows(
-				IllegalArgumentException.class,
-				() -> createUserUseCase.execute(null)
-		);
-		
-		assertEquals("User cannot be null", exception.getMessage());
-		
-		verifyNoInteractions(repository);
-	}
-	
-	@Test
-	@DisplayName("execute() - when already exists email, throw IllegalArgumentException('Email already exists: + " +
-			"email')")
-	void execute_WhenEmailAlreadyExists_ShouldThrowException() {
-		when(repository.existsByEmail(user.getEmail())).thenReturn(true);
-		
-		IllegalArgumentException exception = assertThrows(
-				IllegalArgumentException.class,
-				() -> createUserUseCase.execute(user)
-		);
-		
-		assertEquals("Email already exists: " + user.getEmail(), exception.getMessage());
-		
-		verify(repository, never()).save(any(User.class));
-	}
-	
-	@Test
-	@DisplayName("execute() - when already exists username, throw IllegalArgumentException('Username already exists:" +
-			" " + "+" + " " + "username')")
-	void execute_WhenUsernameAlreadyExists_ShouldThrowException() {
-		when(repository.existsByUsername(user.getUsername())).thenReturn(true);
-		
-		IllegalArgumentException exception = assertThrows(
-				IllegalArgumentException.class,
-				() -> createUserUseCase.execute(user)
-		);
-		
-		assertEquals("Username already exists: " + user.getUsername(), exception.getMessage());
-		
-		verify(repository, never()).save(any(User.class));
-	}
+
+  @Mock
+  private UserRepository repository;
+
+  @Mock
+  private PasswordProvider passwordProvider;
+
+  static private User user;
+
+  @InjectMocks
+  private CreateUserUseCase createUserUseCase;
+
+  @BeforeAll
+  static void initAll() {
+    user = User.builder()
+        .username("johndoe")
+        .firstName("John")
+        .lastName("Doe")
+        .email("johndoe@gmail.com")
+        .password("password123")
+        .build();
+  }
+
+  @Test
+  @DisplayName("execute() - when valid user is provided, should save and return the user")
+  public void execute_WhenValidUser_ShouldSaveAndReturnUser() {
+    when(repository.existsByEmail(user.getEmail())).thenReturn(false);
+    when(repository.existsByUsername(user.getUsername())).thenReturn(false);
+    when(passwordProvider.hashPassword(any(String.class))).thenReturn("hashedPassword");
+
+    user.setPassword("hashedPassword");
+    when(repository.save(user)).thenReturn(user);
+
+    User result = createUserUseCase.execute(user);
+
+    assertNotNull(result);
+    assertEquals(result, user);
+
+    verify(repository, times(1)).existsByEmail(user.getEmail());
+    verify(repository, times(1)).existsByUsername(user.getUsername());
+    verify(passwordProvider, times(1)).hashPassword(user.getPassword());
+    verify(repository, times(1)).save(user);
+  }
+
+  @Test
+  @DisplayName("execute() - when user is null, throw IllegalArgumentException('User cannot be null')")
+  public void execute_WhenInValidUser_ShouldSaveAndReturnUser() {
+    UserCannotNullException exception = assertThrows(
+        UserCannotNullException.class,
+        () -> createUserUseCase.execute(null)
+    );
+
+    assertEquals("User cannot be null", exception.getMessage());
+
+    verifyNoInteractions(repository);
+  }
+
+  @Test
+  @DisplayName(
+      "execute() - when already exists email, throw IllegalArgumentException('Email already exists: + "
+          +
+          "email')")
+  void execute_WhenEmailAlreadyExists_ShouldThrowException() {
+    when(repository.existsByEmail(user.getEmail())).thenReturn(true);
+
+    EmailExistsException exception = assertThrows(
+        EmailExistsException.class,
+        () -> createUserUseCase.execute(user)
+    );
+
+    assertEquals("Email already exists: " + user.getEmail(), exception.getMessage());
+
+    verify(repository, never()).save(any(User.class));
+  }
+
+  @Test
+  @DisplayName(
+      "execute() - when already exists username, throw IllegalArgumentException('Username already exists:"
+          +
+          " " + "+" + " " + "username')")
+  void execute_WhenUsernameAlreadyExists_ShouldThrowException() {
+    when(repository.existsByUsername(user.getUsername())).thenReturn(true);
+
+    UserNameExistsException exception = assertThrows(
+        UserNameExistsException.class,
+        () -> createUserUseCase.execute(user)
+    );
+
+    assertEquals("Username already exists: " + user.getUsername(), exception.getMessage());
+
+    verify(repository, never()).save(any(User.class));
+  }
 }
