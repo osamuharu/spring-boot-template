f5651c19f7c5d205a7ee8b72b2fa45142a5736ad
osamuharu <huynhnamkha1010@gmail.com>
2026-04-08 20:56:21 +0700

    chore: remove slf4j

--- core/src/main/java/com/osamuharu/core/filter/JwtFilter.java
+++ core/src/main/java/com/osamuharu/core/filter/JwtFilter.java
@@ -9,7 +9,6 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import lombok.NonNull;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -22,7 +21,6 @@ import org.springframework.web.filter.OncePerRequestFilter;
 
 @Component
 @RequiredArgsConstructor
-@Slf4j
 public class JwtFilter extends OncePerRequestFilter {
 
   private final UserDetailsService userDetailsService;
