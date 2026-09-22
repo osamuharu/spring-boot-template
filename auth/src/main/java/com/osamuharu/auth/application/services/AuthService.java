6f7b40cf32cbb217ed7212876e662ea907546832
osamuharu <huynhnamkha1010@gmail.com>
2026-09-22 15:13:46 +0700

    feat: move email sending logic to RegisterUseCase for improved separation of concerns

--- auth/src/main/java/com/osamuharu/auth/application/services/AuthService.java
+++ auth/src/main/java/com/osamuharu/auth/application/services/AuthService.java
@@ -10,10 +10,8 @@ import com.osamuharu.auth.presentation.dto.responses.LoginResponseDto;
 import com.osamuharu.security.dtos.PayloadDto;
 import com.osamuharu.security.dtos.TokenDto;
 import com.osamuharu.security.ports.TokenPort;
-import com.osamuharu.shared.dtos.SendMailMessageSimpleDto;
 import com.osamuharu.user.domain.entities.User;
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -26,20 +24,12 @@ public class AuthService {
   private final LogoutUseCase logoutUseCase;
   private final AuthMapper mapper;
   private final TokenPort tokenPort;
-  private final ApplicationEventPublisher eventPublisher;
+
 
   @Transactional
   public void register(RegisterRequestDto dto) {
 
     registerUseCase.execute(mapper.toDto(dto));
-
-    eventPublisher.publishEvent(
-        SendMailMessageSimpleDto.builder()
-            .to(dto.getEmail())
-            .subject("Welcome " + dto.getUsername())
-            .text("Welcome to our service, " + dto.getUsername() + "!")
-            .build()
-    );
   }
 
   public LoginResponseDto login(LoginRequestDto dto) {
--- auth/src/main/java/com/osamuharu/auth/application/useCases/RegisterUseCase.java
+++ auth/src/main/java/com/osamuharu/auth/application/useCases/RegisterUseCase.java
@@ -1,15 +1,26 @@
 package com.osamuharu.auth.application.useCases;
 
+import com.osamuharu.shared.dtos.SendMailMessageSimpleDto;
 import com.osamuharu.user.application.services.UserService;
 import com.osamuharu.user.presentation.dto.requests.CreateUserDto;
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.ApplicationEventPublisher;
 
 @RequiredArgsConstructor
 public class RegisterUseCase {
 
   private final UserService userService;
+  private final ApplicationEventPublisher eventPublisher;
 
   public void execute(CreateUserDto dto) {
     userService.createUser(dto);
+
+    eventPublisher.publishEvent(
+        SendMailMessageSimpleDto.builder()
+            .to(dto.getEmail())
+            .subject("Welcome " + dto.getUsername())
+            .text("Welcome to our service, " + dto.getUsername() + "!")
+            .build()
+    );
   }
 }
